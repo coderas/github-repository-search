f7f87b1965966d4487e73625fa16189d8c836267
coderas <codified@outlook.com>
2020-10-05 16:29:30 +0100

    create apolloClient with Provider and auth

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # GitHub Repository Search
 
 _Bootstrapped using `create-react-app`_
+
+This application will need authentication from GitHub. In order to achieve this, it might be best to generate a new token, and then run the following replacing `XYZABC123` with your copied token:
+
+```
+echo REACT_APP_GITHUB_ACCESS_TOKEN=XYZABC123 > .env.local
+```
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import 'antd/dist/antd.css';
+import { Repositories } from './containers';
 
-function App() {
-  return (
-    <p>App will go here</p>
-  );
-}
+import {
+  ApolloClient,
+  createHttpLink,
+  ApolloProvider,
+  InMemoryCache,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
+
+const token = process.env.REACT_APP_GITHUB_ACCESS_TOKEN;
+
+const httpLink = createHttpLink({
+  uri: GITHUB_BASE_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const App = () => (
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <Repositories />
+    </ApolloProvider>
+  </React.StrictMode>
+);
 
 export default App;
