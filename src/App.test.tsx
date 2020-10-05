7e34fb245a449a9722258568e807201ff8317716
coderas <codified@outlook.com>
2020-10-05 14:56:30 +0100

    added tslint

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-test("renders learn react link", async () => {
+test('renders learn react link', async () => {
   const { getByText } = render(<App />);
   expect(await getByText(/App will go here/i)).toBeInTheDocument();
 });
