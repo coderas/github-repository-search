1d30cb5c19ed63ce601a3eecf1a4216065da8f39
coderas <codified@outlook.com>
2020-10-05 14:50:46 +0100

    stripped out some boilerplate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", async () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(await getByText(/App will go here/i)).toBeInTheDocument();
 });
