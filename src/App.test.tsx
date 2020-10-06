ec17fdda6b0bff7693b8a42cae6ec50f008013f1
Rich Gorman <coderas@users.noreply.github.com>
2020-10-06 09:40:24 +0100

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', async () => {
+test('renders the app', async () => {
   const { getByText } = render(<App />);
   expect(await getByText(/Loading/i)).toBeInTheDocument();
 });
